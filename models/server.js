4a4dd7a7e487ad83a0cae4a529ebcc56cd595106
LuisDavid-Morocho <davidmorocho14@hotmail.com>
2021-08-09 18:41:20 -0500

    first commit

---
+++ models/server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const cors = require('cors');
+const { dbConnection } = require('../database/config');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        //Conectar a base de datos
+
+        this.conectarDB();
+
+        //Middelwares
+        this.middlewares();
+        //Rutas de mi Aplicacion
+        this.routes();
+    }
+
+    async conectarDB() {
+        await dbConnection();
+    }
+
+    middlewares() {
+
+        //CORS
+        this.app.use( cors());
+
+        //Lectura y Parseo del body
+        this.app.use(express.json());
+
+        //Directorio Público
+        this.app.use( express.static('public'));
+
+    }
+
+    routes() {
+        
+        this.app.use('/api/usuarios', require('../routes/usuarios'));
+
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port)
+        })
+    }
+
+}
+
+module.exports = Server;
